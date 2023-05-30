b127cbb5182a4294d1090b627b74b6d0964e0ef2
faridmustafazade <mustafazadefaridm@gmail.com>
2023-05-30 12:08:12 +0400

    first commit

--- frontend/src/schema/CardForm.jsx
+++ frontend/src/schema/CardForm.jsx
@@ -6,11 +6,11 @@ export const CardForm = yup
     image: yup.string().required("Enter the image"),
     name: yup.string().required("Enter the name"),
     about: yup.string().required("Enter the about"),
-    bedrooms: yup.number().required("Enter the bedrooms"),
-    bathrooms: yup.number().required("Enter the bathrooms"),
-    area: yup.number().required("Enter the area"),
-    patio: yup.number().required("Enter the patio"),
-    garage: yup.number().required("Enter the garage"),
-    price: yup.number().required("Enter the price"),
+    bedrooms: yup.string().required("Enter the bedrooms"),
+    bathrooms: yup.string().required("Enter the bathrooms"),
+    area: yup.string().required("Enter the area"),
+    patio: yup.string().required("Enter the patio"),
+    garage: yup.string().required("Enter the garage"),
+    price: yup.string().required("Enter the price"),
   })
   .required();
